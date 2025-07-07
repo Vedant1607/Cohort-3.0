92c2dc20a41d13e84b2597b498fd30dfd34164b5
Vedant1607 <sinhavedant0705@gmail.com>
2025-07-08 02:32:33 +0530

    week-9

---
+++ Classes/9-week/9-3-react-from-basics/2-useState/src/App.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+
+function App() {
+
+  return (
+    <div style={{ backgroundColor: "#bdc3c7", height: "100vh" }}>
+      <ToggleMessage/>
+      <ToggleMessage/>
+      <ToggleMessage/>
+    </div>
+  )
+}
+
+const ToggleMessage = () => {
+    const [isVisible, setIsVisible] = useState(false); // defining a new state variable
+    // When the value of a state variable changes,
+    // the component that uses the state variables re-renders
+
+    return (
+        <div>
+            <button onClick={() => setIsVisible(!isVisible)}>
+                Toggle Message
+            </button>
+            {isVisible && <p>This message is conditionally rendered!</p>}
+        </div>
+    );
+};
+
+
+// 1. starting a react project locally, components, conditional rendering
+/*{ <div style={{ display: "flex", justifyContent:"center"}}>
+        <div>
+          <div>
+            <PostComponent
+            name={"harkirat"}
+            subtitle={"20 followers"}
+            time={"2m ago"}
+            image={"/public/freeCodeCamp.jpg"}
+            description={"What to know how to win big? Check out how these folks won $6000 in bounties."}
+            />
+            <br/> 
+          </div>
+          <div>
+            <PostComponent
+            name={"harkirat"}
+            subtitle={"Promoted"}
+            image={"/public/freeCodeCamp.jpg"}
+            description={"What to know how to win big? Check out how these folks won $6000 in bounties."}
+            />
+            <br/> 
+          </div>
+        // </div>}*/
+      // </div>
+
+// const style = { width: 200, backgroundColor:"white", borderRadius:10, borderColor:"grey", borderWidth:1,
+//   padding:20
+//  };
+/*
+// function PostComponent({name, subtitle, time, image, description}) {
+
+//   return (
+//     <div style={style}>
+//       <div style={{ display: "flex"}}>
+//         <img src={image}
+//         style={{
+//           width:50,
+//           height:50,
+//           borderRadius:20
+//         }}/>
+
+//         <div style={{ fontSize: 10, marginLeft: 10 }}>
+//           <b>{name}</b>
+//           <div>{subtitle}</div>
+//           {(time !== undefined) ? <div style={{display:"flex"}}>
+//             <div>{time}</div>
+//             <img src='https://imgs.search.brave.com/qFyOweU5pSU8nXPP_Pj_g0sClgE1rUU1JZV5rXXgxpE/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzEyLzg5LzM1LzY0/LzM2MF9GXzEyODkz/NTY0NTlfWkRkTlpm/MllPdTdKb0J2TXVN/bkptVEtDMVNwTDJs/Wk0uanBn'
+//             style={{width:30, height:20}}/>
+//           </div>:null}
+//         </div>
+
+//       </div>
+//       <div style={{ fontSize: 12 }}>
+//         {description}
+//       </div>
+//     </div>
+//   )
+// }
+
+// structuring your app into components
+// defining a state of your application*/
+
+export default App
