da3590514879aa002dac2389dc14851bf8f16cc6
Vedant1607 <sinhavedant0705@gmail.com>
2025-07-28 00:34:55 +0530

    week-11:custom-hooks:useFetch

--- Classes/week-11/11-1-custom-hooks/11-1-2-useFetch/src/App.jsx
+++ Classes/week-11/11-1-custom-hooks/11-1-2-useFetch/src/App.jsx
@@ -1,14 +1,29 @@
 import { useEffect, useState } from 'react'
-import { usePostTitle } from './hooks/useFetch';
+import { usePostTitle, useFetch } from './hooks/useFetch';
 
 function App() {
 
+  const [currentPost, setCurrentPost] = useState(1);
   const postTitle = usePostTitle();
+
+  const { finalData, loading } = useFetch(`https://jsonplaceholder.typicode.com/posts/${currentPost}`);
+
+  if (loading) {
+    return <div>
+      Loading...
+    </div>
+  }
 
   return (
     <>
       <div>
         {postTitle}
+      </div>
+      <div>
+        <button onClick={() => setCurrentPost(1)}>1</button>
+        <button onClick={() => setCurrentPost(2)}>2</button>
+        <button onClick={() => setCurrentPost(3)}>3</button>
+        {JSON.stringify(finalData)}
       </div>
     </>
   )
