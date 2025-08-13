72c16a5ef0a5975dc306fa1204d133e7b7b7a70b
Vedant1607 <sinhavedant0705@gmail.com>
2025-08-13 21:54:19 +0530

    week-14:typescript-advance:pick-api

--- week-14/14-3-typescript-advance/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-;
-function sumOfAge(user1, user2) {
-    return user1.age + user2.age;
-}
-const age = sumOfAge({ name: "taro", age: 20 }, { name: "jiro", age: 30 });
-console.log(age);
-//# sourceMappingURL=index.js.map
